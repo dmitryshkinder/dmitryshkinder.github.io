9b11a49b75e057f7e2eb1504aa7566d058f79106
Dmitry Shkinderd <44124141+dmitryshkinder@users.noreply.github.com>
2020-04-29 20:37:45 +0300

    feat: add 2 task

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,33 @@ function drawChess() {
   const containerBlock = document.querySelector('.container');
   let block;
   let flag = true;
+  let numbers = 1;
+  const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'];
+  let pointer = 0;
 
-  for (let i = 0; i < 8; i++) {
-    for (let j = 0; j < 8; j++) {
-      if (j === 0) flag = !flag;
+  for (let row = 0; row < 9; row++) {
+    for (let col = 0; col < 9; col++) {
+      if (col === 1) flag = !flag;
 
       block = document.createElement('div');
 
-      if (flag) block.className = 'block black-block';
-      else block.className = 'block white-block';
+      if (row === 0 && col !== 0) {
+        block.className = 'block boardNumbers';
+        block.innerText = numbers++;
+      } else if (col === 0 && row !== 0) {
+        block.className = 'block boardNumbers';
+        block.innerText = letters[pointer++];
+      } else if (row === 0 && col === 0) {
+        block.className = 'block boardNumbers';
+      } else if (flag) {
+        block.className = 'block white-block';
+        flag = !flag;
+      } else {
+        block.className = 'block black-block';
+        flag = !flag;
+      }
 
       containerBlock.appendChild(block);
-      flag = !flag;
     }
   }
 }
